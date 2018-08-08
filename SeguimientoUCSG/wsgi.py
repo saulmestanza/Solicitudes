0ba9c817bf43ccbc9b0cecb6c09442d8160a765b
Saul Mestanza <saul.mm92@gmail.com>
2018-08-08 15:08:47 -0500

    cambio wsgi

--- SeguimientoUCSG/wsgi.py
+++ SeguimientoUCSG/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for SeguimientoUCSG project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
-import os
+import os, sys, site
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SeguimientoUCSG.settings")
+# os.environ["DJANGO_SETTINGS_MODULE"] = "SeguimientoUCSG.settings"
+
+try:
+    # Add the site packages, to override any system-wide packages
+    site.addsitedir('/var/www/SeguimientoUCSG/venv/lib/python2.7/site-packages')
+    
+    # Activate the virtualenv
+    activate_this = os.path.expanduser(os.path.abspath('/var/www/SeguimientoUCSG/venv/bin/activate_this.py'))
+    execfile(activate_this, dict(__file__=activate_this))
+except Exception as e:
+    print e
 
 application = get_wsgi_application()
